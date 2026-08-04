83ccd99eca3c721ac48dd0206ad2c1ed6520d706
PB <1978026-paulbarre@users.noreply.gitlab.com>
2026-08-04 17:50:23 +0900

    Create front page

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,5 @@
 <template>
   <UApp>
-    <UPageHero title="Jagra" />
+    <NuxtPage />
   </UApp>
 </template>
---
+++ app/pages/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const rules = [
+  { title: '〜に限らず' },
+  { title: '〜につき〈理由〉' },
+]
+</script>
+
+<template>
+  <UContainer>
+    <UPageHero title="Jagra" />
+    <UPageGrid>
+      <UPageCard
+        v-for="(rule, index) in rules"
+        :key="`rule-${index}`"
+        v-bind="rule"
+        variant="subtle"
+      />
+    </UPageGrid>
+  </UContainer>
+</template>
