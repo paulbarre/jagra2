198ebd253067d41ad6f9fe9957d33ab97c974633
PB <1978026-paulbarre@users.noreply.gitlab.com>
2026-08-13 13:31:00 +0900

    Change dev server port to 3100

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+  devServer: { port: 3100 },
   devtools: { enabled: true },
   modules: ['@nuxt/ui'],
   css: ['~/assets/css/main.css']
